d4936290edf5705b8b8285c4034fae92a99bfa57
VikashJain2 <vikkijain991@gmail.com>
2025-03-09 13:24:12 +0530

    created the hero section and features section

---
+++ middleware.js
@@ -0,0 +1,34 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+// Define protected routes
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/resume(.*)",
+  "/interview(.*)",
+  "/ai-cover-letter(.*)",
+  "/onboarding(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  // Get both userId and redirectToSignIn from the auth object
+  
+  const { userId, redirectToSignIn } = await auth();
+
+  // If no userId and the route is protected, redirect to sign-in
+  if (!userId && isProtectedRoute(req)) {
+    return redirectToSignIn();
+  }
+
+  // Continue with the next middleware/response
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
